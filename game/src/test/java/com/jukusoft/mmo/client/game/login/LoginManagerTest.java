f6326090586634bc71ee98d88bea0befb1edcc34
JuKu <kuenzel.justin@t-online.de>
2018-05-21 17:04:22 +0200

    added some junit tests.

--- game/src/test/java/com/jukusoft/mmo/client/game/login/LoginManagerTest.java
+++ game/src/test/java/com/jukusoft/mmo/client/game/login/LoginManagerTest.java
@@ -1,5 +1,6 @@
 package com.jukusoft.mmo.client.game.login;
 
+import io.vertx.core.Handler;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -30,6 +31,87 @@ public class LoginManagerTest {
 
         instance.loggedIn.set(true);
         assertEquals(true, instance.isLoggedIn());
+    }
+
+    @Test (expected = IllegalStateException.class)
+    public void testLoginWithoutExecutor () {
+        LoginManager.getInstance().loginExecutor = null;
+
+        LoginManager.getInstance().login("user", "password", new Handler<LoginManager.LOGIN_RESPONSE>() {
+            @Override
+            public void handle(LoginManager.LOGIN_RESPONSE event) {
+                //
+            }
+        });
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testNullUser () {
+        LoginManager.getInstance().loginExecutor = new Handler<LoginManager.LoginRequest>() {
+            @Override
+            public void handle(LoginManager.LoginRequest event) {
+                //
+            }
+        };
+
+        LoginManager.getInstance().login(null, "password", createHandler());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testEmptyUser () {
+        LoginManager.getInstance().loginExecutor = new Handler<LoginManager.LoginRequest>() {
+            @Override
+            public void handle(LoginManager.LoginRequest event) {
+                //
+            }
+        };
+
+        LoginManager.getInstance().login("", "password", createHandler());
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testNullPassword () {
+        LoginManager.getInstance().loginExecutor = new Handler<LoginManager.LoginRequest>() {
+            @Override
+            public void handle(LoginManager.LoginRequest event) {
+                //
+            }
+        };
+
+        LoginManager.getInstance().login("user", null, createHandler());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testEmptyPassword () {
+        LoginManager.getInstance().loginExecutor = new Handler<LoginManager.LoginRequest>() {
+            @Override
+            public void handle(LoginManager.LoginRequest event) {
+                //
+            }
+        };
+
+        LoginManager.getInstance().login("user", "", createHandler());
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testNullHandler () {
+        LoginManager.getInstance().loginExecutor = new Handler<LoginManager.LoginRequest>() {
+            @Override
+            public void handle(LoginManager.LoginRequest event) {
+                //
+            }
+        };
+
+        LoginManager.getInstance().login("user", "password", null);
+    }
+
+    protected Handler<LoginManager.LOGIN_RESPONSE> createHandler () {
+        return new Handler<LoginManager.LOGIN_RESPONSE>() {
+            @Override
+            public void handle(LoginManager.LOGIN_RESPONSE event) {
+                //
+            }
+        };
     }
 
 }
