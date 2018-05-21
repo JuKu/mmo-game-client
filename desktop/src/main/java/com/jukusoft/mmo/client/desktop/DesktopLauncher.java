89c260ba3cc5a938da563ba4fe58e4ab6b9ae395
JuKu <kuenzel.justin@t-online.de>
2018-05-21 14:48:46 +0200

    added support to show gui window.

--- desktop/src/main/java/com/jukusoft/mmo/client/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/jukusoft/mmo/client/desktop/DesktopLauncher.java
@@ -1,11 +1,41 @@
 package com.jukusoft.mmo.client.desktop;
 
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.jukusoft.mmo.client.desktop.config.WindowConfig;
+import com.jukusoft.mmo.client.game.Game;
+import com.jukusoft.mmo.client.gui.GameGUI;
+
+import java.io.IOException;
 
 public class DesktopLauncher {
 
     public static void main (String[] args) {
+        //start game
+        try {
+            start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    protected static void start () throws Exception {
+        //start game
+        Game game = null;
+
+        //TODO: init game
+
+        //TODO: start networking
+
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+        //load window config
+        WindowConfig windowConfig = new WindowConfig("./config/window.cfg");
+        windowConfig.fillConfig(config);
+
+        // start game
+        new Lwjgl3Application(new GameGUI(game), config);
     }
 
 }
--- gui/src/main/java/com/jukusoft/mmo/client/gui/GameGUI.java
+++ gui/src/main/java/com/jukusoft/mmo/client/gui/GameGUI.java
@@ -1,4 +1,49 @@
 package com.jukusoft.mmo.client.gui;
 
-public class GameGUI {
+import com.badlogic.gdx.ApplicationListener;
+import com.jukusoft.mmo.client.game.Game;
+
+public class GameGUI implements ApplicationListener {
+
+    protected final Game game;
+
+    /**
+    * default constructor
+     *
+     * @param game instance of game
+    */
+    public GameGUI (Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
 }
