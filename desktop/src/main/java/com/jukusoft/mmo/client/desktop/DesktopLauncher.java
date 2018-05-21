37db74dfa81d4f0a484f2c5594ee186564327f09
JuKu <kuenzel.justin@t-online.de>
2018-05-21 14:35:49 +0200

    added class WindowConfig.

--- desktop/src/main/java/com/jukusoft/mmo/client/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/jukusoft/mmo/client/desktop/DesktopLauncher.java
@@ -1,9 +1,11 @@
 package com.jukusoft.mmo.client.desktop;
 
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
 public class DesktopLauncher {
 
     public static void main (String[] args) {
-        //
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
     }
 
 }
---
+++ desktop/src/test/java/com/jukusoft/mmo/client/desktop/DesktopLauncherTest.java
@@ -0,0 +1,12 @@
+package com.jukusoft.mmo.client.desktop;
+
+import org.junit.Test;
+
+public class DesktopLauncherTest {
+
+    @Test
+    public void testConstructor () {
+        new DesktopLauncher();
+    }
+
+}
