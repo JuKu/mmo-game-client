5c156a1e163b348a57219fe6c5c629ea07df7860
JuKu <kuenzel.justin@t-online.de>
2018-05-22 21:56:33 +0200

    added some junit tests.

---
+++ gui/src/test/java/com/jukusoft/mmo/client/gui/screens/AnotherDummyScreen.java
@@ -0,0 +1,42 @@
+package com.jukusoft.mmo.client.gui.screens;
+
+import com.jukusoft.mmo.client.game.Game;
+
+public class AnotherDummyScreen implements IScreen {
+
+    @Override
+    public void onStart(Game game) {
+
+    }
+
+    @Override
+    public void onStop(Game game) {
+
+    }
+
+    @Override
+    public void onResume(Game game) {
+
+    }
+
+    @Override
+    public void onPause(Game game) {
+
+    }
+
+    @Override
+    public boolean processInput(Game game) {
+        return false;
+    }
+
+    @Override
+    public void update(Game game) {
+
+    }
+
+    @Override
+    public void draw(Game game) {
+
+    }
+
+}
---
+++ gui/src/test/java/com/jukusoft/mmo/client/gui/screens/OtherDummyScreen.java
@@ -0,0 +1,43 @@
+package com.jukusoft.mmo.client.gui.screens;
+
+import com.jukusoft.mmo.client.game.Game;
+
+public class OtherDummyScreen implements IScreen {
+
+    protected String someVar = "";
+
+    @Override
+    public void onStart(Game game) {
+
+    }
+
+    @Override
+    public void onStop(Game game) {
+
+    }
+
+    @Override
+    public void onResume(Game game) {
+
+    }
+
+    @Override
+    public void onPause(Game game) {
+
+    }
+
+    @Override
+    public boolean processInput(Game game) {
+        return false;
+    }
+
+    @Override
+    public void update(Game game) {
+
+    }
+
+    @Override
+    public void draw(Game game) {
+
+    }
+}
