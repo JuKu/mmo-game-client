e82b6c9a46ccde00f06da75d82cf8efbc20e1284
JuKu <kuenzel.justin@t-online.de>
2018-05-27 17:54:19 +0200

    added support to login, see issue #6 .

--- network/src/main/java/com/jukusoft/mmo/client/network/handler/impl/AuthHandler.java
+++ network/src/main/java/com/jukusoft/mmo/client/network/handler/impl/AuthHandler.java
@@ -4,11 +4,15 @@ import com.jukusoft.mmo.client.engine.logging.LocalLogger;
 import com.jukusoft.mmo.client.game.WritableGame;
 import com.jukusoft.mmo.client.game.login.LoginManager;
 import com.jukusoft.mmo.client.network.NClient;
+import com.jukusoft.mmo.client.network.Protocol;
 import com.jukusoft.mmo.client.network.handler.NetHandler;
 import com.jukusoft.mmo.client.network.utils.MessageUtils;
+import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 
 public class AuthHandler implements NetHandler {
+
+    protected Handler<LoginManager.LOGIN_RESPONSE> loginHandler = null;
 
     public AuthHandler(NClient client) {
         //register login executor
@@ -27,13 +31,33 @@ public class AuthHandler implements NetHandler {
                 req.loginHandler.handle(LoginManager.LOGIN_RESPONSE.CLIENT_ERROR);
             }
 
-            req.loginHandler.handle(LoginManager.LOGIN_RESPONSE.WRONG_CREDENTIALS);
+            this.loginHandler = req.loginHandler;
+            //req.loginHandler.handle(LoginManager.LOGIN_RESPONSE.WRONG_CREDENTIALS);
         });
     }
 
     @Override
     public void handle(Buffer content, byte type, byte extendedType, NClient client, WritableGame game) {
+        if (type == Protocol.MSG_TYPE_AUTH && extendedType == Protocol.MSG_EXTENDED_TYPE_LOGIN_RESPONSE) {
+            LocalLogger.print("login response received.");
 
+            if (this.loginHandler == null) {
+                LocalLogger.warn("received login response, but client hasnt sended login request yet.");
+                return;
+            }
+
+            int userID = content.getInt(Protocol.MSG_BODY_OFFSET);
+
+            if (userID > 0) {
+                //login successful
+                this.loginHandler.handle(LoginManager.LOGIN_RESPONSE.SUCCESSFUL);
+            } else {
+                //login failed
+                this.loginHandler.handle(LoginManager.LOGIN_RESPONSE.WRONG_CREDENTIALS);
+            }
+
+            this.loginHandler = null;
+        }
     }
 
 }
