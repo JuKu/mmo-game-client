cbea2574cf93852807a3767a64496f132a363dd5
JuKu <kuenzel.justin@t-online.de>
2018-05-31 03:05:49 +0200

    changed create character screen, closed #16 .

--- gui/src/main/java/com/jukusoft/mmo/client/gui/screens/impl/character/CreateCharacterScreen.java
+++ gui/src/main/java/com/jukusoft/mmo/client/gui/screens/impl/character/CreateCharacterScreen.java
@@ -179,6 +179,7 @@ public class CreateCharacterScreen implements IScreen {
 
         this.genderButtonGroup = new ButtonGroup<>(maleCheckBox, femaleCheckBox);
 
+        //set toggle mode
         genderButtonGroup.setMaxCheckCount(1);
         genderButtonGroup.setMinCheckCount(1);
 
