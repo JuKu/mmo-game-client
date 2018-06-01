6a5302d581ff8ed44e8e73d02115823b28d66e08
JuKu <kuenzel.justin@t-online.de>
2018-06-01 22:09:49 +0200

    changed ui of loading region screen, see issue #17 .

--- gui/src/main/java/com/jukusoft/mmo/client/gui/screens/impl/game/LoadRegionScreen.java
+++ gui/src/main/java/com/jukusoft/mmo/client/gui/screens/impl/game/LoadRegionScreen.java
@@ -72,7 +72,7 @@ public class LoadRegionScreen implements IScreen {
 
         // Grab the regions from the atlas and create some images
         logo = new Image(atlas.findRegion("logo_large"));
-        loadingFrame = new Image(atlas.findRegion("loading-frame2"));
+        loadingFrame = new Image(atlas.findRegion("loading-frame"));
         loadingBarHidden = new Image(atlas.findRegion("loading-bar-hidden"));
         screenBg = new Image(this.bgTexture);
         loadingBg = new Image(atlas.findRegion("loading-frame-bg"));
@@ -88,7 +88,7 @@ public class LoadRegionScreen implements IScreen {
         stage.addActor(loadingBg);
         stage.addActor(loadingBarHidden);
         stage.addActor(loadingFrame);
-        stage.addActor(logo);
+        //stage.addActor(logo);
 
         //dont accept input while loading region
         Gdx.input.setInputProcessor(null);
@@ -115,7 +115,7 @@ public class LoadRegionScreen implements IScreen {
         logo.setX((width - logo.getWidth()) / 2);
         logo.setY((height - logo.getHeight()) / 2 + yOffset);
 
-        float loadingBarOffsetY = -200;
+        float loadingBarOffsetY = 50/*-200*/;
 
         // Place the loading frame in the middle of the screen
         loadingFrame.setX((stage.getWidth() - loadingFrame.getWidth()) / 2);
